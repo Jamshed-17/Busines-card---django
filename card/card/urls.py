b5be80b005c37594d3816af822ed11d80053f60c
Jamshed <jamshhed@vk.com>
2024-08-06 10:19:29 +0500

    Параметры представлений

--- card/card/urls.py
+++ card/card/urls.py
@@ -1,25 +1,15 @@
-"""
-URL configuration for card project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from first import views
 
 urlpatterns = [
     path('', views.card, name = 'Визитка 1'),
-    path("work", views.work, kwargs={"name":"БРЦБ", "time":"2 месяца"})
+    re_path(r"^user/(?P<name>\D+)/(?P<age>\d+)", views.user),
+    re_path(r"^user/(?P<name>\D+)", views.user),
+    re_path(r"^user", views.user),
+    path("work", views.work, kwargs={"name":"БРЦБ", "time":"2 месяца"}),
+    re_path("HR/Eror/Data", views.Type),
+    re_path("HR/Eror", views.Eror),
+    re_path("HR", views.HR),
 ]
 
--- card/first/views.py
+++ card/first/views.py
@@ -2,7 +2,15 @@ from django.http import HttpResponse
 
 
 def card(request):
-    return HttpResponse("<h1> Визитка 1</h1>")
+    host = request.META["HTTP_HOST"]
+    user_agent = request.META["HTTP_USER_AGENT"]
+    path = request.path
+
+    return HttpResponse(f"""
+    <p>Host: {host}</p>
+    <p>Path: {path}</p>
+    <p>User-agent: {user_agent}</p>
+""")
 
 def work(request, name, time):
     return HttpResponse(f"""
@@ -10,3 +18,15 @@ def work(request, name, time):
     <p>Название: {name}</p>
     <p>Стаж: {time}</p>
 """)
+
+def HR(requst):
+    return HttpResponse("Hello world", headers={"Date": "Sun, 06 Aug 2024 04:33:08"})
+
+def Eror(request):
+    return HttpResponse("Eror", status=435, reason="incorrect data")
+
+def Type(request):
+    return HttpResponse("<h1>Hi!<h1>", content_type="text/plain", charset="utf-8")
+
+def user(request, name="Undefined", age=0):
+    return HttpResponse(f"<h2>Имя: {name} Возраст: {age}<h2>")
